f57fb18108937fdee3c983c9e03717ec2bcc9439
Danil Sheen <dan.80956@gmail.com>
2023-05-28 14:11:34 +0300

    change str-func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def up_str(string):
+    """возвращает строку со всеми заглавными буквами"""
     return string.upper()
+
+
+def up_first_letter(string):
+    """возвращает строку с заглавными первыми буквами"""
+    return string.title()
